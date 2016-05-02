3fc71c6a5608544db3d0f1a8b0c29f9e757f6db6
taherAbdo <taher.ayoub90@gmail.com>
2016-05-02 17:32:04 +0200

    trivial test commit

--- src/main/java/Log4JStudy/log4j2Policies/App2.java
+++ src/main/java/Log4JStudy/log4j2Policies/App2.java
@@ -6,6 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class App2 {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Hello World!");
+		//get the root logger
+
+
 		final Logger logger = LogManager.getLogger("Log4JStudy.log4j2Policies.App2");
 		for (int i = 0; i < 1000; i++) {
 			logger.debug("inside the controller debug :D"+i);
